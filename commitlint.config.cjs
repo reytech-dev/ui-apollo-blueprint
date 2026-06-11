fd513c6ead56deaaaeed3063b804279dd147dfa6
Philipp Reinert <p.reinert@ecoroute.io>
2026-06-11 21:58:37 +0200

    chore(ci): all build deps commit messages

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'test', 'refactor', 'docs', 'perf', 'chore']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'test', 'refactor', 'docs', 'perf', 'chore', 'build']],
     'type-empty': [2, 'never'],
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
